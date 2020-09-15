1d70f53baf74d6da879d19557f6f13ddf3367913
Obaa10 <70761777+Obaa10@users.noreply.github.com>
2020-09-15 01:38:21 +0000

    2

---
+++ app/src/main/java/com/hfad/last/MovieAdapter.java
@@ -0,0 +1,53 @@
+package com.hfad.last;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder> {
+
+    private ArrayList<Movie> movies ;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardView ;
+        public ViewHolder (CardView v){
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public MovieAdapter (ArrayList<Movie> movies){
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_card,parent,false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        TextView name = (TextView) cardView.findViewById(R.id.movie_name);
+        TextView genre = (TextView) cardView.findViewById(R.id.movie_genre);
+        TextView realised_data = (TextView) cardView.findViewById(R.id.movie_realised_data);
+        name.setText(movies.get(position).getName());
+        genre.setText(movies.get(position).getGenre());
+        realised_data.setText(movies.get(position).getData());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+}

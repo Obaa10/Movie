5a1aa792447cec6a45993f211b2bce32601a6c09
Obaa10 <70761777+Obaa10@users.noreply.github.com>
2020-09-15 01:42:24 +0000

    33

--- app/src/main/java/com/hfad/last/MainActivity.java
+++ app/src/main/java/com/hfad/last/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
